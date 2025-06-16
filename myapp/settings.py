478d7bdf79194c4aa39a530f46ef084634cc51b1
furqann76 <furqanbhai23456@gmail.com>
2025-06-16 22:12:42 +0500

    created a attractive well aligned signup page

--- myapp/settings.py
+++ myapp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "blog",
     "crispy_forms",
     "crispy_bootstrap4",
+    "widget_tweaks",
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
