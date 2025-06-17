395ecc34ee8c9d448b04bbf1fb90947d974c5c73
furqann76 <furqanbhai23456@gmail.com>
2025-06-17 09:57:51 +0500

    added phone number field

--- blog/form.py
+++ blog/form.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -9,14 +10,25 @@ class UserRegisterForm(UserCreationForm):
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
     password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
     profile_pic = forms.ImageField(required=False)
+    phone_number = forms.CharField(max_length=15)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2", "profile_pic")
+        fields = (
+            "username",
+            "email",
+            "phone_number",
+            "password1",
+            "password2",
+            "profile_pic",
+        )
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data["email"]
         if commit:
             user.save()
+            user.profile.phone_number = self.cleaned_data["phone_number"]
+            user.profile.profile_pic = self.cleaned_data["profile_pic"]
+            user.profile.save()
         return user
--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ from django.dispatch import receiver
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
         return self.user.username
